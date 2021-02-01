99d3bca2c9226187e8dc0daf0dd8de7c309a8921
Florencia Barreto <fbarreto@spacedev.uy>
2021-02-01 20:33:05 -0300

    added header

---
+++ src/utils/theme.js
@@ -0,0 +1,12 @@
+export const palette = {
+  beige: '#cfb8a8',
+  brown: '#412a18',
+  grey: '#c4bbb4',
+  lightPink: '#f1e0d8',
+  pink: '#ca877e',
+};
+
+export const typography = {
+  semiBold: 600,
+  bold: 700,
+};
